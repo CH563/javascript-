3ee83c586057e64a9f73c1749a5a44ef6718a872
chenliwen <chenliwen@hzed.com>
2018-04-13 09:40:03 +0800

    update

--- mvvmDemo/demo-mvvm.js
+++ mvvmDemo/demo-mvvm.js
@@ -9,6 +9,9 @@ function Mvvm(options = {}) {
 
   // 数据劫持
   observe(data)
+
+  // 编译
+  new Compile(options.el, this)
 }
 
 /*
@@ -53,3 +56,71 @@ function observe(data) {
   if (!data || typeof data !== 'object') return
   return new Observe(data)
 }
+
+// 创建Compile构造函数
+function Compile(el, vm) {
+  // 将el挂载到实例上方更调用
+  vm.$el = document.querySelector(el)
+  // 在el范围里将内容都拿到，当然不能一个一个的拿
+  // 可以选择移到内存中去然后放入文档碎片中，节省开销
+  let fragment = document.createDocumentFragment()
+
+  while (child = vm.$el.firstChild) {
+    fragment.appendChild(child)
+  }
+  // 对el里面的内容进行替换
+  function replace(frag) {
+    Array.from(frag.childNodes).forEach(node => {
+      let txt = node.textContent
+      let reg = /\{\{(.*)\}\}/
+
+      if (node.nodeType === 3 && reg.test(txt)) { // 即是文本节点又有大括号的情总
+        console.log(RegExp.$1) // 匹配到的第一个分组 如：a.b，c
+        let arr = RegExp.$1.split('.')
+        let val = vm
+        arr.forEach(key => {
+          val = val[key]  // 如this.a.b
+        })
+        // 用trim方法除一下首尾空格
+        node.textContent = txt.replace(reg, val).trim()
+      }
+      // 如果还有子节点，继续递归replace
+      if (node.childNodes && node.childNodes.length) {
+        replace(node)
+      }
+    })
+  }
+  replace(fragment) // 替换内容
+  vm.$el.appendChild(fragment) // 再将文档碎片放
+}
+
+// 发布订阅模式 订阅和发布 如[fn1, fn2, fn3]
+function Dep() {
+  // 一个数组
+  this.subs = []
+}
+Dep.prototype = {
+  addSub(sub) {
+    this.subs.push(sub)
+  },
+  notify() {
+    // 绑定的方法，都有一个updata方法
+    this.subs.forEach(sub => sub.update())
+  }
+}
+
+// 监听函数
+// 通过watch这个类型创建的实例，都拥有update方法
+function Watcher(fn) {
+  this.fn = fn  // 将fn放到实例上
+}
+Watcher.prototype.update = function () {
+  this.fn()
+}
+
+let watcher = new Watcher(() => console.log(111))
+let dep = new Dep()
+// 将watcher放到数组中，watcher自带update方法， => [watcher]
+dep.addSub(watcher)
+dep.addSub(watcher)
+dep.notify()
